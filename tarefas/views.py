f8a515c3fc290a4f8e2ab648a6a7aede827acf7b
João Daniel <jdkklcd1234@gmail.com>
2025-03-31 21:26:03 -0300

    Vercion-1.4
    Foi criado o db e foi adicionado ao html para exibir as informações do db

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import Tarefas
 # Create your views here.
 
 def listaTarefas(request):
-    return render (request, 'tarefas/list.html')
+    tarefas = Tarefas.objects.all().order_by('-created_at')
+    return render (request, 'tarefas/list.html', {'tarefas':tarefas})
