8369deff1829e24bec66e9ce16d6197dc1209154
João Daniel <jdkklcd1234@gmail.com>
2025-04-02 21:49:06 -0300

    Vercion-1.6

--- tarefas/static/js/script.js
+++ tarefas/static/js/script.js
@@ -1,1 +1,17 @@
-console.log("Funcionol");
+// função que pergunta para o usuario se ele quer mesmo escluir a tarefa
+
+document.querySelectorAll(".delete-btn").forEach(
+    btn => {
+        btn.addEventListener("click", function(e){
+            e.preventDefalt();
+        
+            const delLink = this.getAttribute("href");
+
+            if(confirm("Vossa ecelencia gostaria mesmo de apagar essa tarefa?")){
+
+                window.location.href = delLink;
+            }
+            console.log("Olá Mundo")
+        });
+    }
+)
--- tarefas/urls.py
+++ tarefas/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.listaTarefas, name='lista-tarefa'),
     path('tarefa/<int:id>', views.tarefaView,name='tarefa-view'),
     path('novaTarefa/', views.novaTarefa, name='nova-tarefa'),
+    path('edit/<int:id>',views.editTarefa, name='edit-tarefa'),
+    path('delete/<int:id>',views.deletetarefa, name='delete-tarefa'),
 ]
